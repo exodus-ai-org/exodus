98715bb630340e6dd834affa41121e2208a1a1f1
YanceyOfficial <yanceyofficial@gmail.com>
2025-04-03 19:56:22 +0800

    feat(wip): support code preview

--- src/renderer/components/artifacts/code-preview.tsx
+++ src/renderer/components/artifacts/code-preview.tsx
@@ -1,13 +1,13 @@
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
   SheetHeader,
   SheetTitle
 } from '@/components/ui/sheet'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { showArtifactSheetAtom } from '@/stores/chat'
 import { useAtom } from 'jotai'
+import { ReactCodeRunner } from './code-runner'
+import { expamleCode } from './example-code'
 
 export function CodePreview() {
   const [showArtifactSheet, setShowArtifactSheet] = useAtom(
@@ -17,21 +17,29 @@ export function CodePreview() {
     <Sheet open={showArtifactSheet} onOpenChange={setShowArtifactSheet}>
       <SheetContent side="right" className="w-[800px] min-w-[800px] p-4">
         <SheetHeader>
-          <SheetTitle>Edit profile</SheetTitle>
-          <SheetDescription>
-            Make changes to your profile here. Click save when you&apos;re done.
-          </SheetDescription>
+          <SheetTitle>Canvas</SheetTitle>
         </SheetHeader>
         <section className="flex-1">
           {/* <header className="flex items-center justify-between"></header> */}
-          <Tabs defaultValue="code" className="h-full w-[800px]">
-            <TabsList>
-              <TabsTrigger value="code">Code</TabsTrigger>
-              <TabsTrigger value="preview">Preview</TabsTrigger>
-            </TabsList>
-            <TabsContent value="code">CODE</TabsContent>
-            <TabsContent value="preview">PREVEW</TabsContent>
-          </Tabs>
+
+          <ReactCodeRunner code={expamleCode} />
+
+          {/* <SandpackProvider template="react-ts">
+            <SandpackLayout>
+              <Tabs defaultValue="code" className="h-full w-[800px]">
+                <TabsList>
+                  <TabsTrigger value="code">Code</TabsTrigger>
+                  <TabsTrigger value="preview">Preview</TabsTrigger>
+                </TabsList>
+                <TabsContent value="code">
+                  <SandpackCodeEditor className="overscroll-y-scroll h-[calc(100vh-8rem)]" />
+                </TabsContent>
+                <TabsContent value="preview">
+                  <SandpackPreview className="overscroll-y-scroll h-[calc(100vh-8rem)]" />
+                </TabsContent>
+              </Tabs>
+            </SandpackLayout>
+          </SandpackProvider> */}
         </section>
         {/* <SheetFooter>
           <SheetClose asChild>
