914f158fd50a9c62f1055bd18f73d5797976c3b5
YanceyOfficial <yanceyofficial@gmail.com>
2025-05-22 15:27:30 +0800

    perf: just use electron to render pdf

--- src/main/lib/server/routes/tools.ts
+++ src/main/lib/server/routes/tools.ts
@@ -1,19 +1,40 @@
 import { Variables } from '@shared/types/server'
+import { BrowserWindow } from 'electron'
 import { Hono } from 'hono'
-import { mdToPdf } from 'md-to-pdf'
+import MarkdownIt from 'markdown-it'
 
 const tools = new Hono<{ Variables: Variables }>()
 
+async function markdownStringToPdfBuffer(markdownString: string) {
+  const md = new MarkdownIt()
+  const html = md.render(markdownString)
+
+  const win = new BrowserWindow({
+    show: false, // Don't show the window
+    webPreferences: {
+      nodeIntegration: false, // Recommended for security
+      contextIsolation: true // Recommended for security
+    }
+  })
+
+  try {
+    await win.loadURL(
+      `data:text/html;charset=utf-8,${encodeURIComponent(html)}`
+    )
+    const buffer = await win.webContents.printToPDF({
+      pageSize: 'A4'
+    })
+    return buffer
+  } catch (e) {
+    throw e instanceof Error ? e.message : new Error(`Failed to generate PDF`)
+  } finally {
+    win.close()
+  }
+}
+
 tools.post('/md-to-pdf', async (c) => {
   const { markdown } = await c.req.json()
-  const { content } = await mdToPdf({ content: markdown })
-
-  const buffer = Buffer.from(
-    content.buffer.slice(
-      content.byteOffset,
-      content.byteOffset + content.byteLength
-    )
-  )
+  const buffer = await markdownStringToPdfBuffer(markdown)
   return new Response(buffer, {
     headers: {
       'Content-Type': 'application/pdf'
