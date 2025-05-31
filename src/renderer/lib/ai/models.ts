8b8a6702b6be3f78e8a99385c7323fc954ac1690
YanceyOfficial <yanceyofficial@gmail.com>
2025-05-31 22:44:10 +0800

    feat: update models for google ai

--- src/renderer/lib/ai/models.ts
+++ src/renderer/lib/ai/models.ts
@@ -24,10 +24,9 @@ export const models = {
     reasoningModel: ['o4-mini', 'o3-mini', 'o3', 'o1', 'o1-pro']
   },
   [Providers.GoogleGemini]: {
-    chatModel: ['gemini-2.5-flash-preview-04-17', 'gemini-2.0-flash'],
+    chatModel: ['gemini-2.5-flash-preview-05-20', 'gemini-2.0-flash'],
     reasoningModel: [
-      'gemini-2.5-pro-preview-03-25',
-      'gemini-2.5-pro-exp-03-25',
+      'gemini-2.5-pro-preview-05-06',
       'gemini-2.0-flash-thinking-exp-01-21'
     ]
   },
